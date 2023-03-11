f593e907eb12d64a1f029b086b61b343462d4dcc
Johnny Gérard <mail@jgerard.dev>
2023-03-11 01:35:43 +0100

    feat: all sections but the footer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
+
+type Skill = Readonly<{
+  name: string;
+  years: number;
+}>;
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.scss"],
 })
 export class AppComponent {
-  title = 'FEM-Single-page-developer-portfolio';
+  protected readonly skills: readonly Skill[] = [
+    { name: "HTML", years: 4 },
+    { name: "CSS", years: 4 },
+    { name: "JavaScript", years: 4 },
+    { name: "Accessibility", years: 4 },
+    { name: "React", years: 3 },
+    { name: "Sass", years: 3 },
+  ];
 }
