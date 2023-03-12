7103203b47367dbcad062567e0dada45b0ac83c4
Johnny Gérard <mail@jgerard.dev>
2023-03-12 04:13:48 +0100

    style: format document

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,13 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
 
-import { AppComponent } from './app.component';
-import { ProjectComponent } from './project/project.component';
+import { AppComponent } from "./app.component";
+import { ProjectComponent } from "./project/project.component";
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    ProjectComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent, ProjectComponent],
+  imports: [BrowserModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
